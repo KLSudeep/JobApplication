b7e74a93766eafa5d5cffd93ff7b19ac41cd2b35
KLSudeep <sudeepkl2003@gmail.com>
2025-07-01 22:51:57 +0530

    JobApp Project Structure setup

---
+++ src/test/java/com/sudeep/JobApp/JobAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sudeep.JobApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
